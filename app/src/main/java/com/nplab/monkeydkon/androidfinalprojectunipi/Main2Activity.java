ee2677e6f443b799529521bb5c5c7a22f0d10e3d
monkeydkon <kon_fu2@hotmail.com>
2018-02-05 19:26:12 +0200

    first try

--- app/src/main/java/com/nplab/monkeydkon/androidfinalprojectunipi/Main2Activity.java
+++ app/src/main/java/com/nplab/monkeydkon/androidfinalprojectunipi/Main2Activity.java
@@ -9,11 +9,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Main2Activity extends AppCompatActivity {
 
     EditText editText, editText2, editText3, editText4, editText5, editText6, editText7;
     TextView textView;
     Button button5;
+
+    private DatabaseReference mDatabase;
+
+
 
     int cars= 0;
 
@@ -21,13 +28,19 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        editText = (EditText)findViewById(R.id.editText);
-        editText2 = (EditText)findViewById(R.id.editText2);
-        editText3 = (EditText)findViewById(R.id.editText3);
-        editText4 = (EditText)findViewById(R.id.editText4);
-        editText5 = (EditText)findViewById(R.id.editText5);
-        editText6 = (EditText)findViewById(R.id.editText6);
-        editText7 = (EditText)findViewById(R.id.editText7);
+        editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editText2);
+        editText3 = findViewById(R.id.editText3);
+        editText4 = findViewById(R.id.editText4);
+        editText5 = findViewById(R.id.editText5);
+        editText6 = findViewById(R.id.editText6);
+        editText7 = findViewById(R.id.editText7);
+        textView = findViewById(R.id.textView);
+        button5 = findViewById(R.id.button5);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
 
 
 
@@ -49,9 +62,24 @@ public class Main2Activity extends AppCompatActivity {
     //REGISTER
     public void signUp(View view){
 
+        String firstName = editText.getText().toString();
+        String lastName = editText2.getText().toString();
+        String userName = editText3.getText().toString();
+        String passWord = editText4.getText().toString();
+        String carNumber1 = editText5.getText().toString();
+
+        if(firstName.equals("") || lastName.equals("")|| userName.equals("") || passWord.equals("") || carNumber1.equals(""))
+        {
+            Toast.makeText(this,"EMPTY",Toast.LENGTH_LONG).show();
+        }else{
+
+            mDatabase.child("users").child(userName).setValue(firstName);
+
+        }
+
 
         // TO CODE
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this,MainActivity.class);
+        //startActivity(intent);
     }
 }
