ab934feb35a25b45f7e07f4309e5afa6efe57c27
monkeydkon <kon_fu2@hotmail.com>
2018-02-05 21:28:02 +0200

    register almost done

--- app/src/main/java/com/nplab/monkeydkon/androidfinalprojectunipi/Main2Activity.java
+++ app/src/main/java/com/nplab/monkeydkon/androidfinalprojectunipi/Main2Activity.java
@@ -1,16 +1,25 @@
 package com.nplab.monkeydkon.androidfinalprojectunipi;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -20,6 +29,7 @@ public class Main2Activity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
 
+    String value;
 
 
     int cars= 0;
@@ -62,32 +72,90 @@ public class Main2Activity extends AppCompatActivity {
     //REGISTER
     public void signUp(View view){
 
-        String firstName = editText.getText().toString();
-        String lastName = editText2.getText().toString();
-        String userName = editText3.getText().toString();
-        String passWord = editText4.getText().toString();
-        String carNumber1 = editText5.getText().toString();
-        String carNumber2 = editText6.getText().toString();
-        String carNumber3 = editText7.getText().toString();
+        final String firstName = editText.getText().toString();
+        final String lastName = editText2.getText().toString();
+        final String userName = editText3.getText().toString();
+        final String passWord = editText4.getText().toString();
+        final String carNumber1 = editText5.getText().toString();
+        final String carNumber2 = editText6.getText().toString();
+        final String carNumber3 = editText7.getText().toString();
+
+
+
+
+
+/*
+       ValueEventListener postListener = new ValueEventListener() {
+           @Override
+           public void onDataChange(DataSnapshot dataSnapshot) {
+               value = String.valueOf(dataSnapshot.child("users").getValue(String.class));
+           }
+
+           @Override
+           public void onCancelled(DatabaseError databaseError) {
+
+           }
+       }; */
+
+
+
+
+
+
 
         if(firstName.equals("") || lastName.equals("")|| userName.equals("") || passWord.equals("") || carNumber1.equals(""))
         {
-            Toast.makeText(this,"You left a field empty. Try again",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"You left a field/fields empty. Try again",Toast.LENGTH_SHORT).show();
         }else{
 
-            mDatabase.child("users").child(userName).child("firstname").setValue(firstName);
-            mDatabase.child("users").child(userName).child("lastname").setValue(lastName);
-            mDatabase.child("users").child(userName).child("password").setValue(passWord);
-            mDatabase.child("users").child(userName).child("car numbers").child("car number1").setValue(carNumber1);
-            mDatabase.child("users").child(userName).child("car numbers").child("car number2").setValue(carNumber2);
-            mDatabase.child("users").child(userName).child("car numbers").child("car number3").setValue(carNumber3);
+            mDatabase.child("users").child(userName).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()){
+                        Toast.makeText(getApplicationContext(),"This username is already taken. You can try another one!",Toast.LENGTH_SHORT).show();
+                        editText.setText("");
+                        editText2.setText("");
+                        editText3.setText("");
+                        editText4.setText("");
+                        editText5.setText("");
+                        editText6.setText("");
+                        editText7.setText("");
+
+                    }else{
+
+                        mDatabase.child("users").child(userName).child("firstname").setValue(firstName);
+                        mDatabase.child("users").child(userName).child("lastname").setValue(lastName);
+                        mDatabase.child("users").child(userName).child("password").setValue(passWord);
+                        mDatabase.child("users").child(userName).child("car numbers").child("car number1").setValue(carNumber1);
+                        mDatabase.child("users").child(userName).child("car numbers").child("car number2").setValue(carNumber2);
+                        mDatabase.child("users").child(userName).child("car numbers").child("car number3").setValue(carNumber3);
+                        Toast.makeText(getApplicationContext(),"You successfully signed up",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
+
+
 
 
         }
 
 
-     
-        //Intent intent = new Intent(this,MainActivity.class);
-        //startActivity(intent);
+
+
+    }
+
+    private void showData(DataSnapshot dataSnapshot) {
+
+
+
     }
 }
