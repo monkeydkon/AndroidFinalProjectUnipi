30d60481a3ca8e1b534051a6325cef66463827cb
monkeydkon <kon_fu2@hotmail.com>
2018-02-05 18:42:16 +0200

    first try

--- app/src/main/java/com/nplab/monkeydkon/androidfinalprojectunipi/Main2Activity.java
+++ app/src/main/java/com/nplab/monkeydkon/androidfinalprojectunipi/Main2Activity.java
@@ -4,10 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+
+    EditText editText, editText2, editText3, editText4, editText5, editText6, editText7;
+    TextView textView;
+    Button button5;
 
     int cars= 0;
 
@@ -15,17 +21,24 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        editText = (EditText)findViewById(R.id.editText);
+        editText2 = (EditText)findViewById(R.id.editText2);
+        editText3 = (EditText)findViewById(R.id.editText3);
+        editText4 = (EditText)findViewById(R.id.editText4);
+        editText5 = (EditText)findViewById(R.id.editText5);
+        editText6 = (EditText)findViewById(R.id.editText6);
+        editText7 = (EditText)findViewById(R.id.editText7);
+
+
 
     }
 
     // SHOW MORE EDITTEXTS FOR EXTRA CAR NUMBER
     public void moreCars(View view){
         if (cars == 0) {
-            EditText editText6 = (EditText) findViewById(R.id.editText6);
             editText6.animate().alpha(1.0f);
             cars = 1;
         }else if (cars == 1){
-            EditText editText7 = (EditText) findViewById(R.id.editText7);
             editText7.animate().alpha(1.0f);
             cars += 1;
         }else{
